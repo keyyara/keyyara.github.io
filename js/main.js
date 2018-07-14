69736e0f58de76a7a11ea2c9f8cbeaafe37f6eaf
Chiara <cpuccistudio>
2018-07-14 11:38:29 -0400

    Parallax Changes

--- js/main.js
+++ js/main.js
@@ -12,50 +12,102 @@ $('#project-numbers').hover(
   $('#one').hover(
     function () {
     $('#ICO').addClass('show');
-  },
+    $('#ICO-pImage').removeClass('hide');
+    },
     function () {
     $('#ICO').removeClass('show');
+    $('#ICO-pImage').addClass('hide');
   });
 
   $('#two').hover(
     function () {
     $('#Atom').addClass('show');
+    $('#Atom-pImage').removeClass('hide');
   },
     function () {
     $('#Atom').removeClass('show');
+    $('#Atom-pImage').addClass('hide');
   });
 
   $('#three').hover(
     function () {
     $('#Bloc').addClass('show');
+    $('#Bloc-pImage').removeClass('hide');
   },
     function () {
     $('#Bloc').removeClass('show');
+    $('#Bloc-pImage').addClass('hide');
   });
 
   $('#four').hover(
     function () {
     $('#BS').addClass('show');
+    $('#BS-pImage').removeClass('hide');
   },
     function () {
     $('#BS').removeClass('show');
+    $('#BS-pImage').addClass('hide');
   });
 
   $('#five').hover(
     function () {
     $('#MIT').addClass('show');
+    $('#MIT-pImage').removeClass('hide');
   },
     function () {
     $('#MIT').removeClass('show');
+    $('#MIT-pImage').addClass('hide');
+  });
+
+  $('#six').hover(
+    function () {
+    $('#Diesel').addClass('show');
+    $('#Diesel-pImage').removeClass('hide');
+  },
+    function () {
+    $('#Diesel').removeClass('show');
+    $('#Diesel-pImage').addClass('hide');
+  });
+
+  $('#seven').hover(
+    function () {
+    $('#Forge').addClass('show');
+    $('#Forge-pImage').removeClass('hide');
+  },
+    function () {
+    $('#Forge').removeClass('show');
+    $('#Forge-pImage').addClass('hide');
+  });
+
+  $('#eight').hover(
+    function () {
+    $('#Noat').addClass('show');
+    $('#Noat-pImage').removeClass('hide');
+  },
+    function () {
+    $('#Noat').removeClass('show');
+    $('#Noat-pImage').addClass('hide');
+  });
+
+  $('#nine').hover(
+    function () {
+    $('#Chiara').addClass('show');
+    $('#Chiara-pImage').removeClass('hide');
+  },
+    function () {
+    $('#Chiara').removeClass('show');
+    $('#Chiara-pImage').addClass('hide');
   });
 
 //* click *//
 $('#one').click(
   function () {
   $('#ICO').addClass('show');
+  $('#ICO-pImage').removeClass('hide');
 },
   function () {
   $('#ICO').removeClass('show');
+  $('#ICO-pImage').addClass('hide');
 });
 
 });
